53a6b1c4207eedf6e24a48cc8bbdf55a9b985507
Gennaro Tedesco <gennarotedesco@gmail.com>
2021-04-13 01:26:54 +0200

    initial commit

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "boilit",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gennaro-tedesco/boilit
+
+go 1.16
+
+require github.com/spf13/cobra v1.1.3
